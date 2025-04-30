e92ff9032d67a2cbe9a8d594b1d13df0296bd71a
Subhash Kumar <subhashk1508@gmail.com>
2025-04-30 14:08:51 +0530

    initial files added

---
+++ src/App.jsx
@@ -0,0 +1,107 @@
+import { useState } from 'react'
+import {InputBox} from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+
+
+function App() {
+
+  const [amount, setAmount] = useState()
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+
+  const options = Object.keys(currencyInfo)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+  
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
+
+  return (
+    <div
+        className="w-full h-screen flex flex-wrap justify-center  bg-cover bg-no-repeat"
+        style={{
+            backgroundImage: `url('https://images.unsplash.com/photo-1536195583959-e9b956d0c603?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+        }}
+    >
+        <div  className="w-full h-auto p-5 backdrop-blur-sm bg-white/30 flex flex-wrap flex-col justify-center items-center my-auto py-2">
+
+            <div className='md:text-6xl animate-pulse text-3xl font-bold text-white py-1'>Currency Converter</div>
+
+            <div
+             style={{
+                animation: 'marquee 12s linear infinite',
+                '@keyframes marquee': undefined 
+            }} 
+             className='md:text-3xl text-xl  font-bold text-blue-950 py-1'>
+            Live Exchange Rate</div>
+
+                <style>
+                {`
+                @keyframes marquee {
+                    0% { transform: translateX(275%); }
+                    100% { transform: translateX(-325%); }
+                }
+                `}
+                </style>
+        </div>
+        
+        <div className="w-full">
+            <div className=" w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        convert()
+                       
+                    }}
+                >
+                    <div  className="w-full mb-1">
+                        <InputBox
+                            label="From"
+                            amount={amount}
+                            currencyOptions={options}
+                            onCurrencyChange={(currency) => {setFrom(currency);
+                             setAmount(amount); }}
+                            // onCurrencyChange={(currency) => setAmount(amount)}
+                            selectCurrency={from}
+                            onAmountChange={(amount) => setAmount(amount)}
+                        />
+                    </div>
+                    <div className="relative w-full h-0.5">
+                        <button
+                            type="button"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                            onClick={swap}
+                        >
+                            swap
+                        </button>
+                    </div>
+                    <div className="w-full mt-1 mb-4">
+                        <InputBox
+                            label="To"
+                            amount={convertedAmount}
+                            currencyOptions={options}
+                            onCurrencyChange={(currency) => setTo(currency)}
+                            selectCurrency={to}
+                            amountDisable
+                        />
+                    </div>
+                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                        Convert {from.toUpperCase()} to {to.toUpperCase()}
+                    </button>
+                </form>
+            </div>
+        </div>
+    </div>
+);
+}
+
+export default App
